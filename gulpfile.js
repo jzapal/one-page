58055342d46a6d584ee3f46950555914a0a30bf3
jan zapal <janek.zapal@gmail.com>
2022-07-18 18:38:46 +0200

    Update of branding

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
+const { series } = require('gulp');
+
 var gulp = require('gulp');
 var useref = require('gulp-useref');
 var cssnano = require('gulp-cssnano');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 
-gulp.task('build', function(){
+function build (){
   return gulp.src('index.html')
     .pipe(useref())
     .pipe(gulpIf('*.js', uglify()))
     // Minifies only if it's a CSS file
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'))
-});
-gulp.task('fonts', function() {
+}
+function fonts () {
   return gulp.src('assets/fonts/**/*')
   .pipe(gulp.dest('dist/fonts'))
-});
+}
 
-gulp.task('default', ['build', 'fonts']);
+
+exports.default = series(build, fonts);
